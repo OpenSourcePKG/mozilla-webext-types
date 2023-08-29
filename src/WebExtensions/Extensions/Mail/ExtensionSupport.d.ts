c1a8b00ec718defe7c61ccc8b8d6fb30dff0d657
Stefan Werfling <stefan.werfling@pegenau.de>
2023-08-29 14:12:48 +0200

    update objects

---
+++ src/WebExtensions/Extensions/Mail/ExtensionSupport.d.ts
@@ -0,0 +1,57 @@
+/**
+ * The object describing the hook the caller wants to register.
+ * @see https://searchfox.org/comm-central/source/mail/modules/ExtensionSupport.jsm#21
+ */
+export interface ESExtensionHook {
+
+    /**
+     * An array of strings of document URLs on which the given callback should run. If not specified, run on all windows.
+     * @member {string[]}
+     */
+    chromeURLs: string[];
+
+    /**
+     * The callback function to run when window loads the matching document.
+     * @param {} win - receive the matching window object
+     */
+    onLoadWindow(win: any): void;
+
+    /**
+     * The callback function to run when window unloads the matching document.
+     * @param {} win - receive the matching window object
+     */
+    onUnloadWindow(win: any): void;
+}
+
+/**
+ * Helper functions for use by extensions that should ease them plug into the application.
+ * @see https://searchfox.org/comm-central/source/mail/modules/ExtensionSupport.jsm
+ */
+export interface ExtensionSupport {
+
+    /**
+     * Register listening for windows getting opened that will run the specified callback function
+     * when a matching window is loaded.
+     * @param {string} aID - Some identification of the caller, usually the extension ID.
+     * @param {ESExtensionHook} aExtensionHook - The object describing the hook the caller wants to register.
+     * @returns {boolean} True if the passed arguments were valid and the caller could be registered. False otherwise.
+     */
+    registerWindowListener(aID: string, aExtensionHook: ESExtensionHook): boolean;
+
+    /**
+     * Unregister listening for windows for the given caller.
+     * @param {string} aID - Some identification of the caller, usually the extension ID.
+     * @returns {boolean} True if the passed arguments were valid and the caller could be unregistered. False otherwise.
+     */
+    unregisterWindowListener(aID: string): boolean;
+
+    /**
+     * @returns {any[]}
+     */
+    openWindows(): any[];
+
+    /**
+     * @returns {number}
+     */
+    registeredWindowListenerCount(): number;
+}
---
+++ src/WebExtensions/Extensions/Parent/TabManager.d.ts
@@ -0,0 +1,43 @@
+import {NativeTab} from './NativeTab';
+import {NativeTabInfo} from './NativeTabInfo';
+import {Tab} from './Tab';
+
+/**
+ * @see https://searchfox.org/comm-central/source/mail/components/extensions/parent/ext-mail.js#1804
+ */
+export interface TabManager {
+
+    /**
+     * Returns a Tab wrapper for the tab with the given ID.
+     * @param {number} tabId - The ID of the tab for which to return a wrapper.
+     * @param {any} _default - The value to return if no tab exists with the given ID.
+     * @returns {Tab|any} The wrapped tab, or the default value.
+     */
+    get<T>(tabId: number, _default?: T): Tab|T;
+
+    /**
+     * If the extension has requested activeTab permission, grant it those permissions for the current.
+     * @param {NativeTabInfo} nativeTabInfo - The native tab for which to grant permissions.
+     */
+    addActiveTabPermission(nativeTabInfo: NativeTabInfo): void;
+
+    /**
+     * Revoke the extension's activeTab permissions for the current inner window of the given native tab.
+     * @param {NativeTabInfo} nativeTabInfo - The native tab for which to revoke permissions.
+     */
+    revokeActiveTabPermission(nativeTabInfo: NativeTabInfo): void;
+
+    /**
+     * Determines access using extension context.
+     * @param {NativeTab} nativeTab - The tab to check access on.
+     * @returns {boolean} True if the extension has permissions for this tab.
+     */
+    canAccessTab(nativeTab: NativeTab): boolean;
+
+    /**
+     * Returns a new Tab instance wrapping the given native tab info.
+     * @param {NativeTabInfo} nativeTabInfo - The native tab for which to return a wrapper.
+     * @returns {Tab} The wrapped native tab
+     */
+    wrapTab(nativeTabInfo: NativeTabInfo): Tab;
+}
